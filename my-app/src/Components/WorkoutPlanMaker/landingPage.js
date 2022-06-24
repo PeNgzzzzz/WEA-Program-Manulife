cf6c0f91ffe961c10463f47b426a262383644ebb
jal2003 <jaldesai715@gmail.com>
2022-06-24 13:40:53 -0400

    https://github.com/PeNgzzzzz/WEA-Program-Manulife.git

--- my-app/src/Components/WorkoutPlanMaker/landingPage.js
+++ my-app/src/Components/WorkoutPlanMaker/landingPage.js
@@ -5,7 +5,8 @@ import './LandingPage.css'
 const LandingPage = () => {
     return(
         <div id='landing-container'>
-            <h1>Lets get you working out.</h1>
+            <h1>- Schedulizer - Management Platform 
+            </h1>
             <h2>More than 90% of people who start working out quit after 3 months. We get it. It’s hard. </h2>
             <p>Let’s make it easy and do the research and planning for you. </p>        </div>
         
